7f61165f672c74db2e35055009aa613c5502f412
Nir Ovadia <ovadia.nir1@gmail.com>
2020-02-14 13:18:29 -0800

    the raven beckons.

---
+++ src/raven.java
@@ -0,0 +1,11 @@
+  
+package artifactory.test;
+
+/**
+ * Hello world!
+ */
+public class raven {
+    public static void main(String[] args) {
+        System.out.println("Hello World,\nThe Raven beckons.");
+    }
+}
